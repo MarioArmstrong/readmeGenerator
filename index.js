fcbeeb446458bd81749add02f638ca700fb3931e
MarioArmstrong <arms.mario@icloud.com>
2022-08-16 20:32:58 -0800

    setting up basic stuff

--- index.js
+++ index.js
@@ -1,13 +1,111 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateREADME = ({
+    project, live, repo, story, motivation, problem, 
+    learn, standout, challengesWins, requirements, 
+    constributors, tutorials}) =>
+   `# Project Name
+    ${project}
+    #Github Live Link:
+    ```
+    ${live}
+    ```
+    #Github Repository Link:
+    ```
+    ${repo}
+    ```
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    ## Description
+    ### User Story:
+    ${story} 
+    ${motivation}. ${problem}. ${learn}. ${standout}. ${challengesWins}.
 
-// TODO: Create a function to initialize app
-function init() {}
+    ## Installation
+    ${requirements}
 
-// Function call to initialize app
-init();
+    ## Usage
+    ```
+    ![](${tutorials})
+    ```
+
+    ## Credits
+    ${constributors}
+    ## Licenses
+
+    ## Tests`;
+
+
+inquirer.prompt = ([
+    {
+    type: 'input',
+    message: 'what is the name of your project?',
+    name: 'project'
+    },
+    {
+    type: 'input',
+    message: 'what is your Github Live project link?',
+    name: 'live'
+    },
+    {
+    type: 'input',
+    message: 'what is your Github repository link?',
+    name: 'repo'
+    },
+    {
+    type: 'input',
+    message: 'what is your user story?"',
+    name: 'story'
+    },
+    {
+    type: 'input',
+    message: 'what was your motivation?',
+    name: 'motivation'
+    },
+    {
+    type: 'input',
+    message: 'what problem did it solve?',
+    name: 'problem'
+    },
+    {
+    type: 'input',
+    message: 'what did you learn?',
+    name: 'learn'
+    },
+    {
+    type: 'input',
+    message: 'What makes your project stand out?',
+    name: 'standout'
+    },
+    {
+    type: 'input',
+    message: 'List some challenges you faced and improvements you hope to make in the future.',
+    name: 'challengesWins'
+    },
+    {
+    type: 'input',
+    message: 'What are the steps required to install this project? Are there any dependencies?',
+    name: 'requirements'
+    },
+    {
+    type: 'input',
+    message: 'Please list all contributors by providing their Github profile links.',
+    name: 'constributors'
+    },
+    {
+    type: 'input',
+    message: 'Please list any tutorials url links used to complete this project.',
+    name: 'tutorials'
+    }
+       
+])
+
+.then((response) => {
+    console.log(response);
+
+    const readmePageContent = generateREADME(response);
+
+    fs.writeFile('README.md', readmePageContent, (err) =>
+    err ? console.err(err) : console.log('Successfully created a README.md file')
+);
+})
